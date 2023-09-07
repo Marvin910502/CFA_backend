fc8509737b63e5349cc0fb646ffe97ef0d4c5e04
marvin <m91urias@gmail.com>
2023-09-07 18:29:01 -0400

    model commit

--- backend/settings.py
+++ backend/settings.py
@@ -29,7 +29,7 @@ SECRET_KEY = env("SECRET_KEY")
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = env("DEBUG")
 
-ALLOWED_HOSTS = [env("ALLOWED_HOSTS")]
+ALLOWED_HOSTS = [env("ALLOWED_HOSTS"), env("ALLOWED_HOSTS_2")]
 
 
 # Application definition
@@ -41,6 +41,7 @@ INSTALLED_APPS = [
     'django.contrib.sessions',
     'django.contrib.messages',
     'django.contrib.staticfiles',
+    'equations',
     'rest_framework',
 ]
 
--- equations/models.py
+++ equations/models.py
@@ -1,3 +1,17 @@
 from django.db import models
 
 # Create your models here.
+
+
+class Vehicle(models.Model):
+    car_registration = models.CharField(max_length=15)
+    color = models.CharField(max_length=50, blank=True)
+    size = models.FloatField(blank=True)
+    KMs_for_liter = models.FloatField(blank=True)
+
+
+class Parking(models.Model):
+    max_spaces = models.IntegerField(default=15)
+    address = models.TextField(blank=True)
+
+
